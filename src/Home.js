04c55d048723f8f6ccc5fc4c60b7ae57251aa4e7
Danilo Domingos <danilo.domingos02@gmail.com>
2021-03-20 17:54:34 -0300

    :construction: Add logic to response.

--- src/Home.js
+++ src/Home.js
@@ -4,21 +4,40 @@ import BlogList from "./BlogList";
 const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
-
-    const handleDelete = (id) => {
-      const newBlogs = blogs.filter((blog) => blog.id !== id);
-      setBlogs(newBlogs);
-    };
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
-      fetch('http://localhost:8000/blogs')
-        .then((res) => res.json())
-        .then((data) => setBlogs(data));
+
+      setTimeout(() => {
+        fetch('http://localhost:8000/blogs')
+        .then((res) => {
+          if(!res.ok)
+          {
+            throw Error('Could not fetch the data for that resource.');
+          }
+
+          return res.json();
+        })
+        .then((data) => {
+          setBlogs(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setError('Could not fetch the data for that resource.');
+          setIsPending(false);
+        });
+
+      }, 1000)
+      
     }, []);
 
     return (
       <div className="home">
-        {blogs && <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/>}
+        {error && <div>{ error }</div>}
+        {isPending && <div>Loading...</div>}
+        {blogs && <BlogList blogs={blogs} title="All blogs"/>}
       </div>
     );
 }
