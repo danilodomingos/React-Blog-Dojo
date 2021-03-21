698df7af68befb7dc5636fa9d713e9cd51404cab
Danilo Domingos <danilo.domingos02@gmail.com>
2021-03-21 12:04:17 -0300

    :construction::art: Add routes.

--- src/Home.js
+++ src/Home.js
@@ -1,45 +1,18 @@
-import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
+  const {data : blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
 
-    useEffect(() => {
-
-      setTimeout(() => {
-        fetch('http://localhost:8000/blogs')
-        .then((res) => {
-          if(!res.ok)
-          {
-            throw Error('Could not fetch the data for that resource.');
-          }
-
-          return res.json();
-        })
-        .then((data) => {
-          setBlogs(data);
-          setIsPending(false);
-          setError(null);
-        })
-        .catch((err) => {
-          setError('Could not fetch the data for that resource.');
-          setIsPending(false);
-        });
-
-      }, 1000)
-      
-    }, []);
-
-    return (
-      <div className="home">
-        {error && <div>{ error }</div>}
-        {isPending && <div>Loading...</div>}
-        {blogs && <BlogList blogs={blogs} title="All blogs"/>}
-      </div>
-    );
+  return (
+    <div className="home">
+      {error && <div>{ error }</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All blogs"/>}
+    </div>
+  );
+  
 }
 
 export default Home;
