d1863f1ba42cba1ba399a113c22ca60dc55ac816
Danilo Domingos <danilo.domingos02@gmail.com>
2021-03-21 13:33:36 -0300

    :construction::art: Add delete blog.

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,23 @@
-import { useParams } from "react-router";
+import { useHistory, useParams } from "react-router";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
   
   const { id } = useParams();
+  const history = useHistory();
+  
   const { data: blog, isPending, error } = useFetch(`http://localhost:8000/blogs/${id}`);
+
+
+  const handleClick = () => {
+
+    fetch(`http://localhost:8000/blogs/${id}`, {
+      method: 'DELETE',
+      headers: { "Content-Type": "application/json" },
+    }).then(() => {
+      history.push('/');
+    });
+  }
 
   return ( 
     <div className="blog-details">
@@ -15,6 +28,7 @@ const BlogDetails = () => {
           <h2>{ blog.title }</h2>
           <p>Written by { blog.author }</p>
           <div>{ blog.body }</div>
+          <button onClick={() => handleClick(blog.id)}>delete</button>
         </article>
       )}
     </div> 
